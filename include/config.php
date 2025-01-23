6b2a029950d2c6a5e47e23fb8e620cb2eb1f1c96
Ahmet Asaad Hammoud <a.h.ahmet.es@gmail.com>
2025-01-23 22:24:37 +0100

    done

---
+++ include/config.php
@@ -0,0 +1,11 @@
+<?php
+defined('server') ? null : define("server", "sql207.infinityfree.com");
+defined('user') ? null : define("user", "if0_38091684");
+defined('pass') ? null : define("pass", "Ah1no2ab3");
+defined('database_name') ? null : define("database_name", "if0_38091684_dental");
+
+// $web_root =  "https://mcguyverja.com/invoice/"; 
+$web_root = $_SERVER['DOCUMENT_ROOT'] . "/";
+
+
+define('web_root', $web_root);
